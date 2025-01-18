6f647858265a0ca6d708f865a062641c01999468
Jandersn01 <jandersonsantos030@gmail.com>
2025-01-18 00:37:54 -0300

    lojas adicionadas

--- src/js/objects.js
+++ src/js/objects.js
@@ -1,12 +1,52 @@
 const lojas = [
     {
-      id: 'rowme',
+      id: 'SoFarAway',
       images: [
-        '../public/imgs/romwe/calca.jpg',
-        '../public/imgs/romwe/moletom.jpg',
-        '../public/imgs/romwe/saia.jpg',
+        '../public/imgs/SoFarAway/tenispreto.jpg',
+        '../public/imgs/SoFarAway/tenisesportebranco.jpg',
+        '../public/imgs/SoFarAway/teniscalcado.jpg',
       ],
-      name: 'RomWe',
+      name: 'SoFarAway',
+    }, {
+        id: 'raza',
+        images: [
+          '../public/imgs/RaZa/vestidobranco.jpg',
+          '../public/imgs/RaZa/vestidorosa.jpg',
+          '../public/imgs/RaZa/vestidopreto.jpg',
+        ],
+        name: 'RaZa',  
+    } , {
+        id: 'todress',
+        images: [
+          '../public/imgs/ToDress/casacobranco.jpg',
+          '../public/imgs/ToDress/casacobege.jpg',
+          '../public/imgs/ToDress/casacopreto.jpg',
+        ],
+        name: 'ToDress',  
+    } , {
+        id: 'watchu',
+        images: [
+          '../public/imgs/Watchu/camisa.jpg',
+          '../public/imgs/Watchu/conjunto.jpg',
+          '../public/imgs/Watchu/shortverde.jpg',
+        ],
+        name: 'Watchu',  
+    } , {
+        id: '+juice',
+        images: [
+          '../public/imgs/+Juice/sutienbege.jpg',
+          '../public/imgs/+Juice/pijama.jpg',
+          '../public/imgs/+Juice/sutianbranco.jpg',
+        ],
+        name: '+Juice',  
+    }, {
+        id: 'caretome',
+        images: [
+          '../public/imgs/CareToMe/perfumemarrom.jpg',
+          '../public/imgs/CareToMe/perfumeroxo.jpg',
+          '../public/imgs/CareToMe/perfumevinho.jpg',
+        ],
+        name: 'CareToMe',  
     }  ];
   
   export default lojas;
