9209b7640286edfaa2b717cf6b5ee184fbedfa33
Jandersn01 <jandersonsantos030@gmail.com>
2025-01-30 00:44:31 -0300

    changing images paths

--- src/js/objects.js
+++ src/js/objects.js
@@ -129,7 +129,7 @@ const produtos = [
 const promocoes = [
   {
     id: '01',
-    images: 'public/imgs/ofertas/furadeira.jpg',
+    images: '../public/imgs/ofertas/furadeira.jpg',
     name: 'Furadeira Elétrica',
     preco: 'R$305,00',
     promo: 'R$266,99'
@@ -137,28 +137,28 @@ const promocoes = [
   },
   {
     id: '02',
-    images: 'public/imgs/ofertas/camisabeisebol.jpg',
+    images: '../public/imgs/ofertas/camisabeisebol.jpg',
     name: 'Camisa Beisebol',
     preco: 'R$70,00',
     promo: 'R$60,00'
   },
   {
     id: '03',
-    images: 'public/imgs/ofertas/relogiofemino.jpg',
+    images: '../public/imgs/ofertas/relogiofemino.jpg',
     name: 'Relógio Feminino Dourado',
     preco: 'R$25,00',
     promo: 'R$17,65'
   },
   {
     id: '04',
-    images: 'public/imgs/ofertas/pincelmaquiagem.webp',
+    images: '../public/imgs/ofertas/pincelmaquiagem.webp',
     name: 'Pinceis de Maquiagem',
     preco: 'R$20,00',
     promo: 'R$15,00'
   },
   {
     id: '05',
-    images: 'public/imgs/ofertas/boné.jpg',
+    images: '../public/imgs/ofertas/boné.jpg',
     name: 'Camisa Anime',
     preco: 'R$30,00',
     promo: 'R$25,00'
