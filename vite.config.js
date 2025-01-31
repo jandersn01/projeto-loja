b3d014ce66b99a126a792e86cf7f8631c9e76c38
Jandersn01 <jandersonsantos030@gmail.com>
2025-01-30 23:08:19 -0300

    update dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ export default{
     base: '/',
     build: {
         outDir: '../dist',
+        rollupOpitons: {
+            input:{
+                main: '/index.html'
+            }
+        }
     },
 };
